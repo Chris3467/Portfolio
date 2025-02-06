7006e21679745e33492b2b46d00f56a991fa4220
Chris3467 <chris.collado815@gmail.com>
2025-02-05 21:51:39 -0500

    updated

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -51,7 +51,7 @@ function Contact() {
           <p>
             I am interested in freelance opportunities - especially on ambitious
             or large projects. However, if you have any other requests or
-            questions, don't hesitate to contact me using below form either.
+            questions, don't hesitate to contact me using below form.
           </p>
           <div className="contact-form">
             <form ref={form} onSubmit={sendEmail}>
